6631e6b7bd2df07d8fd4efb7e0206af2dcb11845
Unknown <bnmdude@gmail.com>
2018-03-23 23:17:52 +0100

    to be continiued

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const _ = require('lodash');
 const HttpError = require('./error');
